1092242b7fd496c99efa9bbe338ef49f2639d577
urchinly <git@urchinly.uk>
2016-05-12 23:16:23 +0100

    Add about page and legal stuff.

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('status');
+  this.route('stream');
+  this.route('about');
+  this.route('terms');
+  this.route('privacy');
+  this.route('license');
 });
 
 export default Router;
